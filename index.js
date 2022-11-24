5ddf384887a65f2bb766a5015e6bb4871e58bcc5
Jim Reyna <rreyna1982@gmail.com>
2022-11-23 19:38:52 -0500

    imagen carrito

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 //clase producto
 
 class Producto {
-  constructor(id, nombre, precio) {
+  constructor(id, nombre, precio,imagen) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
+    this.imagen = imagen;
   }
 }
 
-const producto1 = new Producto(1, "Mainboard", 300);
-const producto2 = new Producto(2, "Procesador i3", 150);
-const producto3 = new Producto(3, "Procesador i5", 250);
-const producto4 = new Producto(4, "Procesador i7", 300);
-const producto5 = new Producto(5, "Monitor Lg 19", 200);
-const producto6 = new Producto(6, "Monitor Lg 22", 300);
-const producto7 = new Producto(7, "Memoria ddr4 8GB", 30);
-const producto8 = new Producto(8, "Memoria ddr4 16GB", 90);
-const producto9 = new Producto(9, "Case Gamer", 80);
-const producto10 = new Producto(10, "Impresora Epson", 300);
+const producto1 = new Producto(1, "Mainboard", 300,"./img/mainboard.jpg");
+const producto2 = new Producto(2, "Procesador i3", 150,"./img/corei3.jpg");
+const producto3 = new Producto(3, "Procesador i5", 250,"./img/corei5.jpg");
+const producto4 = new Producto(4, "Procesador i7", 300,"./img/corei7.jpg");
+const producto5 = new Producto(5, "Monitor Lg 19", 200,"./img/monitor19.jpg");
+const producto6 = new Producto(6, "Monitor Lg 22", 300,"./img/monitor22.jpg");
+const producto7 = new Producto(7, "Memoria DDR4 8GB", 30,"./img/memoria8.jpg");
+const producto8 = new Producto(8, "Memoria DDR4 16GB", 90,"./img/memoria16.jpg");
+const producto9 = new Producto(9, "Case Gamer Hallion", 80,"./img/casegamer.jpg");
+const producto10 = new Producto(10, "Disco SSD 480GB", 100,"./img/disco480.jpg");
 
 //Creamos Arreglo Pruductos
 const productosArray = [
@@ -39,7 +40,9 @@ const divProductos = document.querySelector("#divProductos");
 
 productosArray.forEach((producto) => {
   divProductos.innerHTML += `
+        
         <div id="${producto.id}" class="card cardProducto">
+        <img id="img-carrito" src="${producto.imagen}" class="card-img-top" alt="...">
         <div class="card-body">
         <h5 class="card-title">${producto.nombre}</h5>
         <p class="card-text">$${producto.precio}</p>
