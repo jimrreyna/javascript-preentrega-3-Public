1964941819ceb7f12f94c91654e4523eed7d57da
Jim Reyna <rreyna1982@gmail.com>
2022-11-27 01:12:15 -0500

    guardamos Json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //clase producto
 
 class Producto {
-  constructor(id, nombre, precio,imagen) {
+  constructor(id, nombre, precio, imagen) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
@@ -9,16 +9,31 @@ class Producto {
   }
 }
 
-const producto1 = new Producto(1, "Mainboard", 300,"./img/mainboard.jpg");
-const producto2 = new Producto(2, "Procesador i3", 150,"./img/corei3.jpg");
-const producto3 = new Producto(3, "Procesador i5", 250,"./img/corei5.jpg");
-const producto4 = new Producto(4, "Procesador i7", 300,"./img/corei7.jpg");
-const producto5 = new Producto(5, "Monitor Lg 19", 200,"./img/monitor19.jpg");
-const producto6 = new Producto(6, "Monitor Lg 22", 300,"./img/monitor22.jpg");
-const producto7 = new Producto(7, "Memoria DDR4 8GB", 30,"./img/memoria8.jpg");
-const producto8 = new Producto(8, "Memoria DDR4 16GB", 90,"./img/memoria16.jpg");
-const producto9 = new Producto(9, "Case Gamer Hallion", 80,"./img/casegamer.jpg");
-const producto10 = new Producto(10, "Disco SSD 480GB", 100,"./img/disco480.jpg");
+const producto1 = new Producto(1, "Mainboard", 300, "./img/mainboard.jpg");
+const producto2 = new Producto(2, "Procesador i3", 150, "./img/corei3.jpg");
+const producto3 = new Producto(3, "Procesador i5", 250, "./img/corei5.jpg");
+const producto4 = new Producto(4, "Procesador i7", 300, "./img/corei7.jpg");
+const producto5 = new Producto(5, "Monitor Lg 19", 200, "./img/monitor19.jpg");
+const producto6 = new Producto(6, "Monitor Lg 22", 300, "./img/monitor22.jpg");
+const producto7 = new Producto(7, "Memoria DDR4 8GB", 30, "./img/memoria8.jpg");
+const producto8 = new Producto(
+  8,
+  "Memoria DDR4 16GB",
+  90,
+  "./img/memoria16.jpg"
+);
+const producto9 = new Producto(
+  9,
+  "Case Gamer Hallion",
+  80,
+  "./img/casegamer.jpg"
+);
+const producto10 = new Producto(
+  10,
+  "Disco SSD 480GB",
+  100,
+  "./img/disco480.jpg"
+);
 
 //Creamos Arreglo Pruductos
 const productosArray = [
@@ -79,15 +94,21 @@ botonesAgregar.forEach((boton) => {
       carrito[indexCarrito].cantidad += 1;
     }
     console.log(carrito);
+    //Guardar json
+    const objCarritoJson = JSON.stringify(carrito);
+    localStorage.setItem("objCarrito", objCarritoJson);
   };
 });
 
+
+
+
 //boton finalizar compra
 
-const botonFinalizar = document.querySelector('#finalizar')
+const botonFinalizar = document.querySelector("#finalizar");
 botonFinalizar.onclick = () => {
   const totalCompra = carrito
     .map((prod) => prod.precio * prod.cantidad)
-    .reduce((a1, b1) => a1 + b1)
-  alert(`El total de su compra es ${totalCompra}`)
-}
+    .reduce((a1, b1) => a1 + b1);
+  alert(`El total de su compra es ${totalCompra}`);
+};
